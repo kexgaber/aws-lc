9f66005a8250a477f8db0d70e8dfdc324d6a8c1b
Keanan Gabertan <kexgaber@amazon.com>
2024-08-02 20:45:28 +0000

    add posix and fips testing

--- .github/docker_images/alpine_linux/entry.sh
+++ .github/docker_images/alpine_linux/entry.sh
@@ -6,13 +6,12 @@
 set -ex -o pipefail
 
 CHECK_FIPS="$1"
-
 if [[ "${CHECK_FIPS}" == "0" ]]; then
     echo "Testing AWS-LC Non-FIPS"
-    tests/ci/run_posix_tests.sh
+    /tests/ci/run_posix_tests.sh
 elif [[ "${CHECK_FIPS}" == "1" ]]; then
     echo "Testing AWS-LC FIPS"
-    tests/ci/run_fips_tests.sh
+    /tests/ci/run_fips_tests.sh
 else
     exit 1
 fi
